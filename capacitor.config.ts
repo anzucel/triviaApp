4ac2ddd7624c5249566533e939858f148d32118c
Zucely Hernandez <andreazucely70@gmail.com>
2021-11-11 12:17:13 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TriviaApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
